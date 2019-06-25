a7c689db7089b8837cb48281dd2a8c0f85ded6d5
felixseriksson <52138884+felixseriksson@users.noreply.github.com>
2019-06-26 00:44:28 +0200

    Update mnist.py

--- examples/mnist.py
+++ examples/mnist.py
@@ -7,18 +7,21 @@ from tensorflow.keras.utils import to_categorical
 mnist = keras.datasets.mnist
 (trainingimages, traininglabels), (testimages, testlabels) = mnist.load_data()
 
-encodedlabels = tf.keras.utils.to_categorical(traininglabels)
+encodedtraininglabels = tf.keras.utils.to_categorical(traininglabels)
+encodedtestlabels = tf.keras.utils.to_categorical(testlabels)
 
 
 classnames = ["0","1","2","3","4","5","6","7","8","9"]
 
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)), 
-    keras.layers.Dense(100, activation=tf.nn.sigmoid), 
+    keras.layers.Dense(100, activation=tf.nn.sigmoid),
+    keras.layers.Dense(100, activation=tf.nn.sigmoid),
     keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 
 model.compile(optimizer="adam", loss="mse", metrics=["accuracy"])
 
-model.fit(trainingimages, encodedlabels, epochs = 5)
+model.fit(trainingimages, encodedtraininglabels, epochs = 200)
+model.evaluate(testimages, encodedtestlabels)
 
