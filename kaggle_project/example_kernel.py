c1ebb126b2959c081d13483dc33404c5285e0999
Hugo Berg <ilykxar@gmail.com>
2019-06-27 09:21:51 +0200

    Implemented basic data generator for out-of-memory training, and furthered work on data validation
    Signed-off-by: Hugo Berg <ilykxar@gmail.com>

--- kaggle_project/example_kernel.py
+++ kaggle_project/example_kernel.py
@@ -15,74 +15,164 @@ from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_a
 
 import sys
 # sys.path.insert(0, '../input')
-
-
 # This is one of the scripts included in the kaggle competition data
 import numpy as np
 
+
+data_dir = os.getcwd() + os.sep + "data" + os.sep
+
+
+class DataLoader(keras.utils.Sequence):
+    def __init__(self, filepaths, batch_size=32, dim=(1024, 1024), shuffle=True):
+        """Initialization"""
+        self.dim = dim
+        self.batch_size = batch_size
+        self.filepaths = filepaths
+        self.indices = np.arange((len(self.filepaths)))
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+    def on_epoch_end(self):
+        """Updates indexes after each epoch"""
+        if self.shuffle:
+            np.random.shuffle(self.indices)
+
+    def __data_generation(self, filepaths):
+        """Generates data containing batch_size samples"""  # X : (n_samples, *dim, n_channels)
+        # Initialization
+        X = np.empty((self.batch_size, *self.dim), dtype=np.uint8)
+        Y = np.empty((self.batch_size, *self.dim), dtype=np.bool)
+
+        # Generate data
+        for i, filepath in enumerate(filepaths):
+            # Store sample
+            X[i,], Y[i] = self.load_filepath(filepath)
+
+        return X, Y
+
+    def load_filepath(self, filepath):
+        train_rle_data = pd.read_csv(os.getcwd() + "/data" + "/train-rle.csv", header=None, index_col=0)
+        dataset = pydicom.dcmread(file_path)
+        X = np.expand_dims(dataset.pixel_array, axis=2)
+        y_raw = str(train_rle_data.loc[file_path.split('/')[-1][:-4], 1])
+
+        if len(y_raw.split()) != 1:
+            Y = np.expand_dims(rle2mask(y_raw, *self.dim).T, axis=2)
+        else:
+            Y = np.zeros((*self.dim, 1))
+
+        return X, Y
+
+    def __len__(self):
+        """Denotes the number of batches per epoch"""
+        return int(np.floor(len(self.filepaths) / self.batch_size))
+
+    def __getitem__(self, index):
+        """Generate one batch of data"""
+        # Generate indexes of the batch
+        indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
+
+        # Find list of IDs
+        batch_shuffled_files = [self.filepaths[k] for k in indexes]
+
+        # Generate data
+        X, Y = self.__data_generation(batch_shuffled_files)
+
+        return X, Y
+
+
+def check_valid_datafile(filepath, rle_df, needs_label=True):
+    # We try to load each file in order to find which ones are invalid
+    try:
+        pydicom.dcmread(filepath, force=True)
+    except Exception as e:
+        print(e)
+        print(f"Skipping loading of {filepath}, file didn't load as a DICOM correctly")
+        return False
+
+    if needs_label:
+        try:
+            str(rle_df.loc[filepath.split('/')[-1][:-4], 1])
+        except:
+            print(f"Skipping loading of {filepath}, file doesn't seem to exist")
+            return False
+    return True
+
+
 def mask2rle(img, width, height):
     rle = []
-    lastColor = 0;
-    currentPixel = 0;
-    runStart = -1;
-    runLength = 0;
+    lastColor = 0
+    currentPixel = 0
+    runStart = -1
+    runLength = 0
 
     for x in range(width):
         for y in range(height):
             currentColor = img[x][y]
             if currentColor != lastColor:
                 if currentColor == 255:
-                    runStart = currentPixel;
-                    runLength = 1;
+                    runStart = currentPixel
+                    runLength = 1
                 else:
-                    rle.append(str(runStart));
-                    rle.append(str(runLength));
-                    runStart = -1;
-                    runLength = 0;
-                    currentPixel = 0;
+                    rle.append(str(runStart))
+                    rle.append(str(runLength))
+                    runStart = -1
+                    runLength = 0
+                    currentPixel = 0
             elif runStart > -1:
                 runLength += 1
-            lastColor = currentColor;
-            currentPixel+=1;
+            lastColor = currentColor
+            currentPixel += 1
 
     return " ".join(rle)
 
+
 def rle2mask(rle, width, height):
-    mask= np.zeros(width* height)
-    array = np.asarray([int(x) for x in rle.split()])
+    mask = np.zeros(width * height)
+    split_data = rle.split()
+    array_data = []
+    for x in split_data:
+        try:
+            array_data.append(int(x))
+        except ValueError:
+            pass
+    array = np.asarray(array_data)
     starts = array[0::2]
     lengths = array[1::2]
+    if len(starts) > len(lengths):
+        starts = starts[:len(lengths)]
 
     current_position = 0
     for index, start in enumerate(starts):
         current_position += start
-        mask[current_position:current_position+lengths[index]] = 255
+        mask[current_position:min(current_position + lengths[index], (width * height) - 1)] = 255
         current_position += lengths[index]
 
     return mask.reshape(width, height)
 
-def show_dcm_info(dataset):
-    print("Filename.........:", file_path)
-    print("Storage type.....:", dataset.SOPClassUID)
-    print()
 
-    pat_name = dataset.PatientName
-    display_name = pat_name.family_name + ", " + pat_name.given_name
-    print("Patient's name......:", display_name)
-    print("Patient id..........:", dataset.PatientID)
-    print("Patient's Age.......:", dataset.PatientAge)
-    print("Patient's Sex.......:", dataset.PatientSex)
-    print("Modality............:", dataset.Modality)
-    print("Body Part Examined..:", dataset.BodyPartExamined)
-    print("View Position.......:", dataset.ViewPosition)
-
-    if 'PixelData' in dataset:
-        rows = int(dataset.Rows)
-        cols = int(dataset.Columns)
-        print("Image size.......: {rows:d} x {cols:d}, {size:d} bytes".format(
-            rows=rows, cols=cols, size=len(dataset.PixelData)))
-        if 'PixelSpacing' in dataset:
-            print("Pixel spacing....:", dataset.PixelSpacing)
+# def show_dcm_info(dataset):
+#     print("Filename.........:", file_path)
+#     print("Storage type.....:", dataset.SOPClassUID)
+#     print()
+#
+#     pat_name = dataset.PatientName
+#     display_name = pat_name.family_name + ", " + pat_name.given_name
+#     print("Patient's name......:", display_name)
+#     print("Patient id..........:", dataset.PatientID)
+#     print("Patient's Age.......:", dataset.PatientAge)
+#     print("Patient's Sex.......:", dataset.PatientSex)
+#     print("Modality............:", dataset.Modality)
+#     print("Body Part Examined..:", dataset.BodyPartExamined)
+#     print("View Position.......:", dataset.ViewPosition)
+#
+#     if 'PixelData' in dataset:
+#         rows = int(dataset.Rows)
+#         cols = int(dataset.Columns)
+#         print("Image size.......: {rows:d} x {cols:d}, {size:d} bytes".format(
+#             rows=rows, cols=cols, size=len(dataset.PixelData)))
+#         if 'PixelSpacing' in dataset:
+#             print("Pixel spacing....:", dataset.PixelSpacing)
 
 
 def plot_pixel_array(dataset, figsize=(10, 10)):
@@ -92,43 +182,15 @@ def plot_pixel_array(dataset, figsize=(10, 10)):
 
 
 if __name__ == "__main__":
-    data_pref = os.getcwd() + os.sep + "data" + os.sep + "dicom-images-train"
+    train_data_pref = data_dir + "dicom-images-train"
+    test_data_pref = data_dir + "dicom-images-test"
 
-    im_height = 1024
-    im_width = 1024
-    num_imgs = 1377
-    im_chan = 1
+    rle_data = pd.read_csv(os.getcwd() + "/data" + "/train-rle.csv", header=None, index_col=0)
+    valid_train_filepaths = [file_path for file_path in
+                             glob(train_data_pref + "/*/*/*.dcm", recursive=True)
+                             if check_valid_datafile(file_path, rle_data)]
+    valid_test_filepaths = [file_path for file_path in
+                             glob(test_data_pref + "/*/*/*.dcm", recursive=True)
+                             if check_valid_datafile(file_path, rle_data, needs_label=False)]
 
-    print('reading input images and mask dataset.....')
-    X_train = np.zeros((num_imgs, im_height, im_width, im_chan), dtype=np.uint8)
-    Y_train = np.zeros((num_imgs, im_height, im_width, 1), dtype=np.bool)
-
-    for q, file_path in enumerate(glob(data_pref+"/*/*/*.dcm", recursive=True)):
-        #print(file_path)
-        df = pd.read_csv(os.getcwd() + "/data" + "/train-rle.csv", header=None, index_col=0)
-        try:
-            dataset = pydicom.dcmread(file_path)
-        except:
-            continue
-        X_train[q] = np.expand_dims(dataset.pixel_array, axis=2)
-        if df.loc[file_path.split('/')[-1][:-4],1] != -1:
-            print(df)
-            Y_train[q] = np.expand_dims(rle2mask(df.loc[file_path.split('/')[-1][:-4],1], 1024, 1024).T, axis=2)
-        else:
-            Y_train[q] = np.zeros((1024, 1024, 1))
-
-    plt.subplot(2, 2, 1)
-    plt.title('input')
-    plt.imshow(X_train[0, :, :, 0], cmap='gray')
-    plt.axis('off')
-
-    plt.subplot(2, 2, 2)
-    plt.title('mask')
-    plt.imshow(Y_train[0, :, :, 0])
-    plt.axis('off')
-
-    plt.subplot(2, 2, 3)
-    plt.title('overlap')
-    plt.imshow(label2rgb(Y_train[0, :, :, 0], image=X_train[0, :, :, 0], alpha=0.3))
-    plt.axis('off')
-    plt.show()
+    print(len(valid_test_filepaths), len(valid_train_filepaths))
