49ecddfd34d4d59c78d24034216ce011736fb5f0
felixseriksson <52138884+felixseriksson@users.noreply.github.com>
2019-07-01 10:33:12 +0200

    changed size

--- kaggle_project/example_kernel.py
+++ kaggle_project/example_kernel.py
@@ -554,7 +554,7 @@ if __name__ == "__main__":
             x, y = train_generator.load_filepath(to_predict)
             pred = model.predict(np.asarray([x]))[0]
             
-            fig = plt.figure(figsize=(2, 2))
+            fig = plt.figure(figsize=(8, 8))
             fig.add_subplot(2, 2, 1)
             plt.imshow(pred.reshape((1024, 1024)), vmin=0, vmax=1)
             fig.add_subplot(2, 2, 2)
